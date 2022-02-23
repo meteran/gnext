4a3d6a886381e50e1b4210f1ac2bd17119ad0213
Piotr Wójcik <piotr_to@op.pl>
2022-02-23 20:52:49 +0100

    simpler initialization of router, with more but hidden options (#11)

--- docs/handler.go
+++ docs/handler.go
@@ -2,7 +2,9 @@ package docs
 
 import (
 	_ "embed"
+	"fmt"
 	"github.com/gin-gonic/gin"
+	"gopkg.in/yaml.v3"
 	"html/template"
 	"net/http"
 )
@@ -13,22 +15,43 @@ var docsTemplateStr string
 type Handler struct {
 	docs     *Docs
 	template *template.Template
+	port     string
 }
 
-func NewHandler(docs *Docs) *Handler {
+func NewHandler(docs *Docs, port string) *Handler {
 	tmpl := template.Must(template.New("").Parse(docsTemplateStr))
-	return &Handler{docs: docs, template: tmpl}
+	return &Handler{docs: docs, template: tmpl, port: port}
 }
 
 func (h *Handler) Docs(ctx *gin.Context) {
 	ctx.Header("Content-Type", "text/html; charset=utf-8")
 	ctx.Status(http.StatusOK)
-	err := h.template.Execute(ctx.Writer, h.docs)
+
+	values := map[string]interface{}{
+		"title":   h.docs.OpenApi.Info.Title,
+		"jsonUrl": fmt.Sprintf("http://localhost:%s%s", h.port, h.docs.JsonUrl),
+	}
+
+	err := h.template.Execute(ctx.Writer, values)
 	if err != nil {
 		panic(err.Error())
 	}
 }
 
-func (h *Handler) File(ctx *gin.Context) {
-	ctx.JSON(http.StatusOK, h.docs.OpenAPIContent())
+func (h *Handler) JsonFile(ctx *gin.Context) {
+	ctx.Header("Content-Description", "File Transfer")
+	ctx.Header("Content-Disposition", fmt.Sprintf("attachment; filename=%s.json", h.docs.OpenApi.Info.Title))
+	ctx.JSON(http.StatusOK, h.docs.OpenApi)
+}
+
+func (h *Handler) YamlFile(ctx *gin.Context) {
+	ctx.Header("Content-Description", "File Transfer")
+	ctx.Header("Content-Disposition", fmt.Sprintf("attachment; filename=%s.yaml", h.docs.OpenApi.Info.Title))
+
+	bytes, err := yaml.Marshal(h.docs.OpenApi)
+	if err != nil {
+		panic(err)
+	}
+
+	ctx.Data(http.StatusOK, "application/x-yaml; charset=utf-8", bytes)
 }
--- docs/structures.go
+++ docs/structures.go
@@ -1,12 +1,7 @@
 package docs
 
 import (
-	"encoding/json"
-	"fmt"
 	"github.com/getkin/kin-openapi/openapi3"
-	"github.com/gin-contrib/cors"
-	"os"
-	"regexp"
 )
 
 const (
@@ -16,95 +11,75 @@ const (
 	JsonTag          = "json"
 	DefaultStatusTag = "default_status"
 	StatusCodesTag   = "status_codes"
+	NoUrl            = "-"
 )
 
-type Docs struct {
-	OpenAPIPath     string
-	OpenAPI         *openapi3.T
-	OpenAPIUrl      string
-	Title           string
-	TermsOfService  string
-	Description     string
-	License         *openapi3.License
-	Contact         *openapi3.Contact
-	Version         string
-	OpenAPIFilePath string
-	InMemory        bool
-	CORS            *cors.Config
+type Options struct {
+	// ExtensionProps OpenAPI extensions.
+	// It reads/writes all properties with prefix "x-".
+	// It is empty as default.
+	ExtensionProps openapi3.ExtensionProps
+
+	// Title of the documentation.
+	// If not set, the default value is "Documentation".
+	Title string
+
+	// Description of the documentation.
+	// It is empty as default.
+	Description string
+
+	// TermsOfService usually should contain the URL to terms of service.
+	// It is empty as default.
+	TermsOfService string
+
+	// Contact information must be in openAPI format.
+	// It is empty as default.
+	Contact *openapi3.Contact
+
+	// License information must be in openAPI format.
+	// It is empty as default.
+	License *openapi3.License
+
+	// Version of the documentation.
+	// If not set, the default value is "1.0.0".
+	Version string
+
+	// InteractiveUrl is the path where your interactive documentation will be placed. It must be an absolute path, started with "/".
+	// If you run the server locally, then your interactive docs will be under "http://localhost:<port><InteractiveUrl>".
+	// If set to NoUrl, the interactive documentation will not be served.
+	// If not set, the default value is "/docs".
+	//
+	// Interactive documentation uses the JSON file, thus it requires the JsonUrl is set to a valid url.
+	// If the JsonUrl is set to NoUrl, the interactive documentation will be disabled.
+	InteractiveUrl string
+
+	// JsonUrl is the path where your openAPI in JSON format will be placed. It must be an absolute path, started with "/".
+	// If you run the server locally, then the file will be under "http://localhost:<port><JsonUrl>".
+	// If set to NoUrl, the json file will not be served.
+	// If not set, the default value is "/docs.json".
+	JsonUrl string
+
+	// YamlUrl is the path where your openAPI in YAML format will be placed. It must be an absolute path, started with "/".
+	// If you run the server locally, then the file will be under "http://localhost:<port><YamlUrl>".
+	// If set to NoUrl, the yaml file will not be served.
+	// If not set, the default value is "/docs.yaml".
+	YamlUrl string
+
+	// Servers is the list of the API locations. In interactive documentation (see InteractiveUrl) you can try your API out using one of those servers.
+	// In case it is an empty list, it will be empty.
+	// In case it is nil, the default value is a list with one element "http://localhost:8080".
+	Servers []string
+
+	// Tags for the documentation.
+	// It is empty as default.
+	Tags []string
 }
 
-func (d *Docs) NewOpenAPI() {
-	d.OpenAPI = &openapi3.T{
-		OpenAPI: "3.0.0",
-		Info: &openapi3.Info{
-			Title:          d.Title,
-			Description:    d.Description,
-			TermsOfService: d.TermsOfService,
-			Contact:        d.Contact,
-			License:        d.License,
-			Version:        d.Version,
-		},
-	}
-}
-
-func (d *Docs) Valid() error {
-	if !d.InMemory && d.OpenAPIFilePath == "" {
-		return fmt.Errorf("invalid docs configuration, OpenAPIFilePath field cannot to be empty without InMemory option")
-	}
-	return nil
-}
-
-func (d *Docs) OpenAPIContent() *openapi3.T {
-	if !d.InMemory {
-		doc, err := openapi3.NewLoader().LoadFromFile(d.OpenAPIFilePath + "openapi.json")
-		if err != nil {
-			panic(fmt.Sprintf("unable to load openapi.json file (path: %s), err: %v", d.OpenAPIPath, err))
-		}
-		return doc
-	}
-	return d.OpenAPI
-}
-
-func (d *Docs) SetPath(path string, method string, doc *Endpoint) {
-	existingPathItem := d.PathItem(d.FixPath(path))
-	existingPathItem.SetOperation(method, (*openapi3.Operation)(doc))
-	if d.PathsIsEmpty() {
-		d.OpenAPI.Paths = make(openapi3.Paths)
-	}
-	d.OpenAPI.Paths[d.FixPath(path)] = existingPathItem
-}
-
-func (d *Docs) SetPathItem(path string, pathItem *openapi3.PathItem) {
-	d.OpenAPI.Paths[d.FixPath(path)] = pathItem
-}
-
-func (d *Docs) AddServer(addr string) {
-	d.OpenAPI.Servers = append(d.OpenAPI.Servers, &openapi3.Server{URL: addr})
-}
-
-func (d *Docs) Build() error {
-	data, err := json.Marshal(d.OpenAPI)
-	if err != nil {
-		return err
-	}
-	// I assume the folder will be created, if it doesn't mean that there is already such a folder.
-	_ = os.Mkdir(d.OpenAPIFilePath, 0755)
-	return os.WriteFile(d.OpenAPIFilePath+"openapi.json", data, 0755)
-}
-
-func (d *Docs) PathsIsEmpty() bool {
-	return len(d.OpenAPI.Paths) == 0
-}
-
-func (d *Docs) FixPath(path string) string {
-	reg := regexp.MustCompile("/:([0-9a-zA-Z]+)")
-	return reg.ReplaceAllString(path, "/{${1}}")
-}
-
-func (d *Docs) PathItem(path string) *openapi3.PathItem {
-	pathItem := d.OpenAPI.Paths.Find(path)
-	if pathItem == nil {
-		return &openapi3.PathItem{}
-	}
-	return pathItem
+var defaultOptions = &Options{
+	Title:          "Documentation",
+	Version:        "1.0.0",
+	InteractiveUrl: "/docs",
+	JsonUrl:        "/docs.json",
+	YamlUrl:        "/docs.yaml",
+	Servers:        []string{"http://localhost:8080"},
 }
