6548a17576193a9e3c88fcc7623bbd4a7b96b2a4
Robert Kruczek <kruczekn1@gmail.com>
2022-12-06 19:53:24 +0100

    changes in tests

--- middleware_test.go
+++ middleware_test.go
@@ -7,69 +7,61 @@ import (
 	"testing"
 )
 
-type handler struct {
-	called bool
-}
-
 func TestBeforeMiddlewareWithParamInHeaders(t *testing.T) {
-	h := &handler{}
+	var called bool
 
 	type middlewareHeaders struct {
 		Headers
 		ContentType string `header:"Content-Type"`
 	}
 
-	middleware := func() Middleware {
-		return Middleware{
-			Before: func(headers *middlewareHeaders) error {
-				if headers.ContentType != "some content type" {
-					return fmt.Errorf("unexpected content type")
-				}
-				return nil
-			},
-		}
+	middleware := Middleware{
+		Before: func(headers *middlewareHeaders) error {
+			if headers.ContentType != "some content type" {
+				return fmt.Errorf("unexpected content type")
+			}
+			return nil
+		},
 	}
 
 	r := Router()
-	r.Use(middleware())
+	r.Use(middleware)
 	r.GET("/foo", func() {
-		h.called = true
+		called = true
 	})
 
 	res := makeRequest(t, r, http.MethodGet, "/foo")
 
 	assert.Equal(t, res.Code, 500)
-	assert.False(t, h.called)
+	assert.False(t, called)
 }
 func TestBeforeMiddlewareReturnSomeContext(t *testing.T) {
-	h := &handler{}
+	var called bool
 
 	type someMiddlewareContext struct {
 		SomeContextValue string
 	}
 
-	middleware := func() Middleware {
-		return Middleware{
-			Before: func() *someMiddlewareContext {
-				return &someMiddlewareContext{SomeContextValue: "test value"}
-			},
-		}
+	middleware := Middleware{
+		Before: func() *someMiddlewareContext {
+			return &someMiddlewareContext{SomeContextValue: "test value"}
+		},
 	}
 
 	r := Router()
-	r.Use(middleware())
+	r.Use(middleware)
 	r.GET("/foo", func(someMiddlewareCtx *someMiddlewareContext) {
-		h.called = true
+		called = true
 		assert.Equal(t, "test value", someMiddlewareCtx.SomeContextValue)
 	})
 
 	res := makeRequest(t, r, http.MethodGet, "/foo")
 
 	assert.Equal(t, 200, res.Code)
-	assert.True(t, h.called)
+	assert.True(t, called)
 }
 func TestBeforeMiddlewareUsingOtherBeforeMiddlewareContext(t *testing.T) {
-	h := &handler{}
+	var called bool
 
 	type firstMiddlewareContext struct {
 		SomeContextValue string
@@ -79,127 +71,139 @@ func TestBeforeMiddlewareUsingOtherBeforeMiddlewareContext(t *testing.T) {
 		SomeContextValue string
 	}
 
-	firstMiddleware := func() Middleware {
-		return Middleware{
-			Before: func() *firstMiddlewareContext {
-				return &firstMiddlewareContext{SomeContextValue: "test value"}
-			},
-		}
+	firstMiddleware := Middleware{
+		Before: func() *firstMiddlewareContext {
+			return &firstMiddlewareContext{SomeContextValue: "test value"}
+		},
 	}
 
-	secondMiddleware := func() Middleware {
-		return Middleware{
-			Before: func(firstMiddlewareCtx *firstMiddlewareContext) *secondMiddlewareContext {
-				return &secondMiddlewareContext{SomeContextValue: firstMiddlewareCtx.SomeContextValue}
-			},
-		}
+	secondMiddleware := Middleware{
+		Before: func(firstMiddlewareCtx *firstMiddlewareContext) (*firstMiddlewareContext, *secondMiddlewareContext) {
+			firstMiddlewareCtx.SomeContextValue = "changed test value"
+			return firstMiddlewareCtx, &secondMiddlewareContext{SomeContextValue: firstMiddlewareCtx.SomeContextValue}
+		},
+	}
+
+	thirdMiddleware := Middleware{
+		Before: func(firstMiddlewareCtx *firstMiddlewareContext, secondMiddlewareCtx *secondMiddlewareContext) {
+			assert.Equal(t, "changed test value", firstMiddlewareCtx.SomeContextValue)
+			assert.Equal(t, "changed test value", secondMiddlewareCtx.SomeContextValue)
+		},
 	}
 
 	r := Router()
-	r.Use(firstMiddleware())
-	r.Use(secondMiddleware())
+	r.Use(firstMiddleware)
+	r.Use(secondMiddleware)
+	r.Use(thirdMiddleware)
 	r.GET("/foo", func(firstMiddlewareCtx *firstMiddlewareContext, secondMiddlewareCtx *secondMiddlewareContext) {
-		h.called = true
-		assert.Equal(t, "test value", firstMiddlewareCtx.SomeContextValue)
-		assert.Equal(t, "test value", secondMiddlewareCtx.SomeContextValue)
+		called = true
+		assert.Equal(t, "changed test value", firstMiddlewareCtx.SomeContextValue)
+		assert.Equal(t, "changed test value", secondMiddlewareCtx.SomeContextValue)
 	})
 
 	res := makeRequest(t, r, http.MethodGet, "/foo")
 
 	assert.Equal(t, 200, res.Code)
-	assert.True(t, h.called)
+	assert.True(t, called)
 }
 func TestAfterMiddlewareUsingSomeTypeFromHandler(t *testing.T) {
-	h := &handler{}
+	var called bool
 
 	type response struct {
 		Response
 		Message string
 	}
 
-	middleware := func() Middleware {
-		return Middleware{
-			After: func(res *response) {
-				assert.Equal(t, "test", res.Message)
-			},
-		}
+	middleware := Middleware{
+		After: func(res *response) {
+			assert.Equal(t, "test", res.Message)
+		},
 	}
 
 	r := Router()
-	r.Use(middleware())
+	r.Use(middleware)
 	r.GET("/foo", func() *response {
-		h.called = true
+		called = true
 		return &response{Message: "test"}
 	})
 
 	res := makeRequest(t, r, http.MethodGet, "/foo")
 
 	assert.Equal(t, res.Code, 200)
-	assert.True(t, h.called)
+	assert.True(t, called)
 }
 func TestPassBeforeMiddlewareContextToAfterMiddleware(t *testing.T) {
-	h := &handler{}
+	var called bool
 
 	type someMiddlewareContext struct {
 		SomeContextValue string
 	}
 
-	middleware := func() Middleware {
-		return Middleware{
-			Before: func() *someMiddlewareContext {
-				return &someMiddlewareContext{SomeContextValue: "test"}
-			},
-			After: func(someMiddlewareCtx *someMiddlewareContext) {
-				assert.Equal(t, "test", someMiddlewareCtx.SomeContextValue)
-			},
-		}
+	middleware := Middleware{
+		Before: func() *someMiddlewareContext {
+			return &someMiddlewareContext{SomeContextValue: "test"}
+		},
+		After: func(someMiddlewareCtx *someMiddlewareContext) {
+			assert.Equal(t, "test", someMiddlewareCtx.SomeContextValue)
+		},
 	}
 
 	r := Router()
-	r.Use(middleware())
+	r.Use(middleware)
 	r.GET("/foo", func(someMiddleWareCtx *someMiddlewareContext) *someMiddlewareContext {
-		h.called = true
+		called = true
 		return someMiddleWareCtx
 	})
 
 	res := makeRequest(t, r, http.MethodGet, "/foo")
 
 	assert.Equal(t, res.Code, 200)
-	assert.True(t, h.called)
+	assert.True(t, called)
 }
 func TestAfterMiddlewareUsingOtherAfterMiddlewareContext(t *testing.T) {
-	h := &handler{}
+	var called bool
 
-	type someMiddlewareContext struct {
+	type firstMiddlewareContext struct {
 		SomeContextValue string
 	}
 
-	firstMiddleware := func() Middleware {
-		return Middleware{
-			After: func(someMiddlewareCtx *someMiddlewareContext) *someMiddlewareContext {
-				return someMiddlewareCtx
-			},
-		}
+	type secondMiddlewareContext struct {
+		SomeContextValue string
 	}
 
-	secondMiddleware := func() Middleware {
-		return Middleware{
-			After: func(someMiddlewareCtx *someMiddlewareContext) {
-				assert.Equal(t, "test", someMiddlewareCtx.SomeContextValue)
-			},
-		}
+	firstMiddleware := Middleware{
+		After: func(firstMiddlewareCtx *firstMiddlewareContext) *firstMiddlewareContext {
+			return firstMiddlewareCtx
+		},
+	}
+
+	secondMiddleware := Middleware{
+		After: func(firstMiddlewareCtx *firstMiddlewareContext) (*firstMiddlewareContext, *secondMiddlewareContext) {
+			assert.Equal(t, "test", firstMiddlewareCtx.SomeContextValue)
+
+			firstMiddlewareCtx.SomeContextValue = "changed test value"
+			return firstMiddlewareCtx, &secondMiddlewareContext{SomeContextValue: firstMiddlewareCtx.SomeContextValue}
+		},
+	}
+
+	thirdMiddleware := Middleware{
+		After: func(firstMiddlewareCtx *firstMiddlewareContext, secondMiddlewareContext *secondMiddlewareContext) {
+			assert.Equal(t, "changed test value", firstMiddlewareCtx.SomeContextValue)
+			assert.Equal(t, "changed test value", secondMiddlewareContext.SomeContextValue)
+		},
 	}
 
 	r := Router()
-	r.Use(firstMiddleware())
-	r.Use(secondMiddleware())
-	r.GET("/foo", func() *someMiddlewareContext {
-		h.called = true
-		return &someMiddlewareContext{SomeContextValue: "test"}
+	r.Use(firstMiddleware)
+	r.Use(secondMiddleware)
+	r.Use(thirdMiddleware)
+	r.GET("/foo", func() *firstMiddlewareContext {
+		called = true
+		return &firstMiddlewareContext{SomeContextValue: "test"}
 	})
 
 	res := makeRequest(t, r, http.MethodGet, "/foo")
 
 	assert.Equal(t, res.Code, 200)
-	assert.True(t, h.called)
+	assert.True(t, called)
 }
